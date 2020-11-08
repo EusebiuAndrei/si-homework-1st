66d0e5b2d66cbc527fb8a9dfedee4ec40137e6cf
EusebiuAndrei <48438191+EusebiuAndrei@users.noreply.github.com>
2020-11-08 16:49:03 +0200

    Delete ex4.py

--- ex4.py
+++
@@ -1,9 +0,0 @@
-def get_song(notes, moves, start):
-    return [notes[(start + move) % len(notes)] for move in moves]
-
-
-if __name__ == '__main__':
-    # notes = [x for x in list(input().split(' '))]
-    # moves = [int(x) for x in list(input().split(' '))]
-    # start = int(input())
-    print(get_song(["do", "re", "mi", "fa", "sol"], [1, -3, 4, 2], 2))
